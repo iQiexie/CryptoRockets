d7cf488990b3403eb84e6cef8870a9d7c1675e76
user <email@email.com>
2025-05-31 11:30:59 +0300

    fix users

--- app/db/migrations/versions/0007.py
+++ app/db/migrations/versions/0007.py
@@ -1,11 +1,13 @@
-"""add indexes
+"""fix default premium
 
 Revision ID: 0007
 Revises: 0006
-Create Date: 2025-05-31 11:23:17.627206
+Create Date: 2025-05-31 11:29:58.103741
 
 """
 from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
 
 revision = '0007'
 down_revision = '0006'
@@ -14,10 +16,24 @@ depends_on = None
 
 
 def upgrade() -> None:
+    op.alter_column(
+        'users', 'tg_is_premium',
+        existing_type=sa.BOOLEAN(),
+        nullable=False
+    )
     op.create_index(op.f('ix_users_referral_from'), 'users', ['referral_from'], unique=False)
-    op.create_foreign_key('users_referral_from_fkey', 'users', 'users', ['referral_from'], ['referral'], ondelete='CASCADE')
+    op.create_unique_constraint('users_referral_unique_key', 'users', ['referral'])
+    op.create_foreign_key(
+        'users_referral_from_fkey', 'users', 'users', ['referral_from'], ['referral'], ondelete='CASCADE'
+    )
 
 
 def downgrade() -> None:
     op.drop_constraint('users_referral_from_fkey', 'users', type_='foreignkey')
+    op.drop_constraint('users_referral_unique_key', 'users', type_='unique')
     op.drop_index(op.f('ix_users_referral_from'), table_name='users')
+    op.alter_column(
+        'users', 'tg_is_premium',
+        existing_type=sa.BOOLEAN(),
+        nullable=True
+    )
