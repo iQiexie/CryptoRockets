cbea65f1b2f30b45c25552bacd8aa53f9dfa09cc
user <email@email.com>
2025-06-07 13:29:09 +0300

    add frontend id

--- app/api/dto/user/response.py
+++ app/api/dto/user/response.py
@@ -1,9 +1,13 @@
+import math
+
 from pydantic import ConfigDict, Field, computed_field
+from pydantic import field_validator
 
 from app.api.dto.base import BaseResponse
 from app.config.constants import BOT_NAME
 from app.config.constants import REFERRAL_PREFIX
 from app.config.constants import WEBAPP_NAME
+from app.db.models import Rocket
 from app.db.models import RocketSkinEnum, RocketTypeEnum
 
 
@@ -11,12 +15,20 @@ class RocketResponse(BaseResponse):
     model_config = ConfigDict(populate_by_name=True, from_attributes=True)
 
     id: int
+    frontend_id: str | None = Field(default=None)
     type: RocketTypeEnum
     fuel_capacity: int
     current_fuel: int
     enabled: bool
     skins: list[RocketSkinEnum]
     current_skin: RocketSkinEnum
+
+    @field_validator("frontend_id", mode="before")
+    @classmethod
+    def validate_frontend_id(cls, v: str | None, values: dict) -> str:
+        if v is None:
+            return str(values.get("id"))
+        return v
 
 
 class UserResponse(BaseResponse):
@@ -28,6 +40,32 @@ class UserResponse(BaseResponse):
     wheel_balance: float
 
     rockets: list[RocketResponse]
+
+    @field_validator("rockets", mode="before")
+    @classmethod
+    def validate_rockets(cls, v: list[Rocket]) -> list[RocketResponse]:
+        resp = []
+
+        for rocket in v:
+            if rocket.current_fuel <= rocket.fuel_capacity:
+                resp.append(RocketResponse.model_validate(rocket))
+                continue
+
+            rocket_count = math.ceil(rocket.current_fuel / rocket.fuel_capacity)
+            for i in range(rocket_count):
+                i += 1
+
+                if rocket.fuel_capacity * i <= rocket.current_fuel:
+                    current_fuel = rocket.fuel_capacity
+                else:
+                    current_fuel = rocket.current_fuel - rocket.fuel_capacity * (i - 1)
+
+                temp_rocket = RocketResponse.model_validate(rocket)
+                temp_rocket.current_fuel = current_fuel
+                temp_rocket.frontend_id = f"{rocket.id}_{i}"
+                resp.append(temp_rocket)
+
+        return resp
 
     @computed_field
     def referral(self) -> str:
