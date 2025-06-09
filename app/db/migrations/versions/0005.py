22d5ece693f856574b077d1a5a6bfa169cf24c20
user <email@email.com>
2025-06-09 17:41:16 +0300

    refactor

---
+++ app/db/migrations/versions/0005.py
@@ -0,0 +1,29 @@
+"""refactor
+
+Revision ID: 0005
+Revises: 0004
+Create Date: 2025-06-09 17:40:29.252929
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = '0005'
+down_revision = '0004'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.alter_column('invoices', 'currency_fee',
+               existing_type=sa.NUMERIC(),
+               nullable=True,
+               existing_server_default=sa.text("'0'::numeric"))
+
+
+def downgrade() -> None:
+    op.alter_column('invoices', 'currency_fee',
+               existing_type=sa.NUMERIC(),
+               nullable=False,
+               existing_server_default=sa.text("'0'::numeric"))
