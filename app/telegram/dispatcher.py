2a9f26ca35ed4ed28ab63ca267934ebcdacd6fd1
user <email@email.com>
2025-06-13 23:28:52 +0300

    fix

--- app/telegram/dispatcher.py
+++ app/telegram/dispatcher.py
@@ -1,6 +1,7 @@
 from aiogram import Dispatcher
 
-from app.telegram.routes import start
+from app.telegram.routes import start, payment
 
 root_dispatcher = Dispatcher()
 root_dispatcher.include_router(start.router)
+root_dispatcher.include_router(payment.router)
